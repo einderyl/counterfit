91d0643ceafbbf4b92db374cafbcefd95bbb1a8b
einderyl <36505414+einderyl@users.noreply.github.com>
2021-08-25 15:43:51 +0800

    ok

--- counterfit/targets/distilbert/distilbert.py
+++ counterfit/targets/distilbert/distilbert.py
@@ -29,7 +29,7 @@ class Distilbert(TextTarget):
 
     def __call__(self, x):
         encoded_dict = self.tokenizer.batch_encode_plus(
-                        x,                      # Sentence to encode.
+                        list(x),                      # Sentence to encode.
                         add_special_tokens = True, # Add '[CLS]' and '[SEP]'
                         max_length = 512,           # Pad & truncate all sentences.
                         truncation = True,
